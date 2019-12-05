586fa1dfdb0cefd0ce69ba9459781e224028471c
hskim <kimzk@semi-neotech.com>
2019-12-05 10:21:28 +0900

    Update change_tag_height_to_twice_width.py
    이미지 크기 변경 스크립터 수정

--- change_tag_height_to_twice_width.py
+++ change_tag_height_to_twice_width.py
@@ -1,15 +1,18 @@
 import xml.dom.minidom
 import os
+import shutil
 
 file_list = os.listdir("./")
 file_list_xml = [file for file in file_list if file.endswith(".xml")]
-print ("file_list_xml: {}".format(file_list_xml))
+#print ("file_list_xml: {}".format(file_list_xml))
 
 for file_xml in file_list_xml:
+    imageFile = file_xml[:-4] + ".jpg"
+    imagePath = "./result/" + imageFile
     resulPath = "./result/" + file_xml
     doc = xml.dom.minidom.parse(file_xml)
     person = doc.getElementsByTagName("object")
-    print("person size:%d" % person.length)
+    #print("person size:%d" % person.length)
     name = doc.getElementsByTagName("name")
     xmin = doc.getElementsByTagName("xmin")
     xmax = doc.getElementsByTagName("xmax")
@@ -17,13 +20,13 @@ for file_xml in file_list_xml:
     ymax = doc.getElementsByTagName("ymax")
 
     for idx in range(0, xmin.length):
-        print("name:%s" % name[idx].firstChild.nodeValue)
+        #print("name:%s" % name[idx].firstChild.nodeValue)
         width = int(xmax[idx].firstChild.nodeValue) - int(xmin[idx].firstChild.nodeValue)
         height = int(ymax[idx].firstChild.nodeValue) - int(ymin[idx].firstChild.nodeValue)
-        print("width:%d, height:%d" % (width, height))
+        #print("width:%d, height:%d" % (width, height))
         if height > width * 2:
             ymax[idx].firstChild.nodeValue = str(width * 2 + int(ymin[idx].firstChild.nodeValue))
-            print("change ymax:%s" % ymax[idx].firstChild.nodeValue)
+            #print("change ymax:%s" % ymax[idx].firstChild.nodeValue)
             if not os.path.exists(os.path.dirname(resulPath)):
                 try:
                     os.makedirs(os.path.dirname(resulPath))
@@ -32,5 +35,25 @@ for file_xml in file_list_xml:
                         raise
             with open(resulPath,'w') as f:
                 f.write(doc.toxml())
+
+            if os.path.exists(imageFile):
+                shutil.copy2(imageFile, imagePath)
+            else:
+                print("No exists img file:%s" % imageFile)
+        elif height == width * 2:
+            #print("retain ymax:%s" % ymax[idx].firstChild.nodeValue)
+            if not os.path.exists(os.path.dirname(resulPath)):
+                try:
+                    os.makedirs(os.path.dirname(resulPath))
+                except OSError as exc: # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        raise
+            with open(resulPath,'w') as f:
+                f.write(doc.toxml())
+
+            if os.path.exists(imageFile):
+                shutil.copy2(imageFile, imagePath)
+            else:
+                print("No exists img file:%s" % imageFile)
         else:
-            continue
+            print("exclude xml file:%s" % file_xml)
